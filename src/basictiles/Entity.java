7a83d362a6f4b2cf4576416f80143bd3dd687a31
kmne68 <kmne68@gmail.com>
2017-06-10 00:49:12 -0500

    Begin entity

--- src/basictiles/Entity.java
+++ src/basictiles/Entity.java
@@ -11,11 +11,32 @@ import java.awt.Image;
 /**
  *
  * @author Keith
+ * 
+ * Add the player entity to the map.
+ * Floating point numbers make for smooth movement on the grid
  */
 public class Entity {
+    
+    private float playerX;
+    private float playerY;
+    private float playerAngle;
+    private float playerSize = 0.3f;
+    private Image spriteImage;
+    private Map map;
 
-    Entity(Image playerSprite, Map map, float f, float f0) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+     * 
+     * @param image
+     * @param map
+     * @param x
+     * @param y 
+     */
+    Entity(Image image, Map map, float x, float y) {
+        
+        this.spriteImage = image;
+        this.map = map;
+        this.playerX = x;
+        this.playerY = y;
     }
 
     void paint(Graphics2D g) {
